1dfa48e479f27098031f9b5c64aa519c5ac56e14
summershower <remonyamano7@gmail.com>
2022-03-03 01:04:49 +0800

    3.3凌晨

--- src/components/Playground/Games/SequenceMemoryGame/index.tsx
+++ src/components/Playground/Games/SequenceMemoryGame/index.tsx
@@ -1,44 +1,160 @@
-import styles from './index.less'
-import { useEffect, useState, useMemo } from 'react'
-export default function SequenceMemoryGame() {
-  const [sequenceArr, setSequenceArr] = useState<number[]>([])
-  const [userTouchSequence, setUserTouchSequence] = useState<number[]>([])
-  const [isNotAllowTouch, setIsNotAllowTouch] = useState(true)
+import styles from './index.less';
+import { useEffect, useState, useRef, useCallback } from 'react';
+export default function SequenceMemoryGame({ restart }: { restart: Function }) {
+  const GAMING = 'gaming';
+  const FAILED = 'failed';
+  const RESULT = 'result';
+  const [state, setState] = useState(GAMING);
 
+  const [sequenceArr, setSequenceArr] = useState<number[]>([]);
+  const [userTouchIndex, setUserTouchIndex] = useState(0);
+  const [isNotAllowTouch, setIsNotAllowTouch] = useState(true);
   function createRandomNumber(): number {
-    const randomNumber = Math.floor(Math.random() * 10)
-    if (randomNumber === 0 || (sequenceArr.length && sequenceArr[length - 1] === randomNumber)) return createRandomNumber()
-    return randomNumber
+    const randomNumber = Math.floor(Math.random() * 10);
+    if (
+      randomNumber === 0 ||
+      (sequenceArr.length && sequenceArr[length - 1] === randomNumber)
+    )
+      return createRandomNumber();
+    return randomNumber;
   }
 
   function touch(index: number) {
-
+    highlightBox(index);
+    if (!isNotAllowTouch) {
+      if (index === sequenceArr[userTouchIndex]) {
+        if (userTouchIndex === sequenceArr.length - 1) {
+          passThisLevel();
+        } else {
+          passThisTouch(index);
+        }
+      } else {
+        failedThisLevel();
+      }
+    }
   }
   async function sleep(ms: number) {
     return new Promise((resolve) => {
       setTimeout(() => {
-        resolve(1)
-      }, ms)
-    })
+        resolve(true);
+      }, ms);
+    });
   }
-  async function showSequence() {
-    await sleep(2000)
-    sequenceArr.forEach(async (v, index) => {
 
-      sleep(500)
-    })
+  const sequenceArrRef = useRef<number[]>([]);
+  sequenceArrRef.current = sequenceArr;
+  async function showSequence() {
+    if (!isNotAllowTouch) setIsNotAllowTouch(true);
+    await sleep(1200);
+    console.log(sequenceArrRef.current);
+    sequenceArrRef.current.forEach((v, index) => {
+      setTimeout(() => {
+        highlightBox(v);
+      }, index * 500);
+      if (index === sequenceArrRef.current.length - 1) {
+        setTimeout(() => {
+          setIsNotAllowTouch(false);
+        }, sequenceArrRef.current.length * 500);
+      }
+    });
+  }
+  async function highlightBox(index: number) {
+    await sleep(1);
+    const targetBox = document.querySelector('#box' + index);
+    if (targetBox) {
+      targetBox.classList.add(styles.highlight);
+      await sleep(200);
+      targetBox.classList.remove(styles.highlight);
+    }
+  }
+
+  const playgroundRef = useRef<HTMLDivElement | null>(null);
+  async function whiteGlitter() {
+    playgroundRef.current &&
+      playgroundRef.current.classList.add(styles.highlight);
+    await sleep(200);
+    playgroundRef.current &&
+      playgroundRef.current.classList.remove(styles.highlight);
+  }
+  async function redGlitter() {
+    playgroundRef.current && playgroundRef.current.classList.add(styles.red);
+    await sleep(200);
+    playgroundRef.current && playgroundRef.current.classList.remove(styles.red);
+  }
+
+  function passThisTouch(index: number) {
+    setUserTouchIndex(userTouchIndex + 1);
+  }
+  function passThisLevel() {
+    whiteGlitter();
+    setUserTouchIndex(0);
+    setSequenceArr([...sequenceArr, createRandomNumber()]);
+    showSequence();
+  }
+  function failedThisLevel() {
+    redGlitter();
+    setState(FAILED);
   }
 
   useEffect(() => {
-    setSequenceArr([createRandomNumber()])
-    showSequence()
-  }, [])
-  return (
-    <div className={styles.playground}>
-      <h2>当前等级：{sequenceArr.length}</h2>
-      <div className={styles.box}>
-        {new Array(9).fill(null).map((v, index) => <div className={styles.eachBox} key={index} onClick={() => touch(index + 1)}></div>)}
+    setSequenceArr([createRandomNumber()]);
+    showSequence();
+  }, []);
+
+  useEffect(() => {
+    console.log('检查刷新');
+  });
+
+  function Gaming() {
+    return (
+      <div>
+        <h2>当前等级：{sequenceArr.length}</h2>
+        <div className={styles.box}>
+          {new Array(9).fill(null).map((v, index) => (
+            <div
+              className={styles.eachBox}
+              id={'box' + (index + 1)}
+              key={index}
+              onClick={() => touch(index + 1)}
+            ></div>
+          ))}
+        </div>
       </div>
+    );
+  }
+  function Failed() {
+    return (
+      <div>
+        <h1>分数是：{sequenceArr.length}</h1>
+        <p>寄了</p>
+        <button onClick={() => restart()}>重开</button>
+      </div>
+    );
+  }
+  function Result() {
+    return (
+      <div>
+        通过了
+        <button>下一关</button>
+      </div>
+    );
+  }
+  function render() {
+    switch (state) {
+      case GAMING:
+        return <Gaming></Gaming>;
+      case FAILED:
+        return <Failed></Failed>;
+      case RESULT:
+        return <Result></Result>;
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <div className={styles.playground} ref={playgroundRef}>
+      {render()}
     </div>
-  )
+  );
 }
--- src/components/Playground/index.tsx
+++ src/components/Playground/index.tsx
@@ -1,35 +1,36 @@
-import styles from './index.less'
-import { IPlaygroundProps } from '@/types'
-import { useState } from 'react'
-import { useLocation } from 'umi'
-import PlaygroundIntro from '@/components/Playground/PlaygroundIntro'
-import ReactionTimeGame from '@/components/Playground/Games/ReactionTimeGame'
-import SequenceMemoryGame from '@/components/Playground/Games/SequenceMemoryGame'
+import styles from './index.less';
+import { IPlaygroundProps } from '@/types';
+import { useState } from 'react';
+import { useLocation } from 'umi';
+import PlaygroundIntro from '@/components/Playground/PlaygroundIntro';
+import ReactionTimeGame from '@/components/Playground/Games/ReactionTimeGame';
+import SequenceMemoryGame from '@/components/Playground/Games/SequenceMemoryGame';
 export default function Playground(props: IPlaygroundProps) {
-  const [isGameStarted, setIsGameStarted] = useState(false)
-  const { pathname } = useLocation()
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const { pathname } = useLocation();
   const gamesRender = () => {
     if (isGameStarted) {
       switch (pathname) {
-        case "/reactiontime":
+        case '/reactiontime':
+          return <ReactionTimeGame></ReactionTimeGame>;
+        case '/sequencememory':
           return (
-            <ReactionTimeGame></ReactionTimeGame>
-          );
-        case "/sequencememory":
-          return (
-            <SequenceMemoryGame></SequenceMemoryGame>
+            <SequenceMemoryGame
+              restart={() => setIsGameStarted(false)}
+            ></SequenceMemoryGame>
           );
       }
-
     } else {
       return (
-        <PlaygroundIntro title={props.intro.title} desc={props.intro.desc} icon={props.intro.icon} link={props.intro.link || undefined} startGame={() => setIsGameStarted(true)}></PlaygroundIntro>
-      )
+        <PlaygroundIntro
+          title={props.intro.title}
+          desc={props.intro.desc}
+          icon={props.intro.icon}
+          link={props.intro.link || undefined}
+          startGame={() => setIsGameStarted(true)}
+        ></PlaygroundIntro>
+      );
     }
-  }
-  return (
-    <div className={styles.playground}>
-      {gamesRender()}
-    </div>
-  )
+  };
+  return <div className={styles.playground}>{gamesRender()}</div>;
 }
