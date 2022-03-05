bde2277349195fbc0807959ced8c3983c41f0b5c
summershower <remonyamano7@gmail.com>
2022-03-06 00:11:01 +0800

    3.6 2

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = 8033;
-app.use(express.static('dist'));
+app.use(express.static('docs'));
 app.listen(port, () => console.log(`Server Start at ${port}`));
